3996e609185976e4b9fbe52642d8587215ab6eac
Misty Malkasian <andrewandmisty@gmail.com>
2019-09-11 12:01:40 -0500

    initial commit with beginnings of Add Tests

---
+++ CustomList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClassProject
+{
+    public class CustomList<T>
+    {
+        // member variables
+        private T[] items;
+        private T[] count 
+       
+            public T[] Count
+            {
+                get
+                {
+                    return count;
+                }
+                set
+                {
+                    count = value;
+                }
+            }
+
+
+
+        // constructor
+        public CustomList()
+        {
+            items = new T[4];
+        }
+
+        // member methods
+    }
+}
